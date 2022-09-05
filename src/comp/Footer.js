ee38e55ee1eb1cd8c3956f2a752ca0ec3a209384
alielrayes <engali7assan@yahoo.com>
2022-09-04 18:25:52 -0700

    lesson-6

---
+++ src/comp/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      Designed and developed by Courses4Arab.com
+      <span>🧡</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/comp/header.js
@@ -0,0 +1,137 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="hide-when-mobile">
+        <h1>c4a.dev</h1>
+        <ul className="flex">
+          <li className="main-list">
+            <a className="main-link" href="/html">
+              HTML
+            </a>
+            {/* <ul className="sub-ul">
+              <li>
+                <a href="">Full Course</a>
+              </li>
+              <li>
+                <a href="">Crash Course</a>
+              </li>
+              <li>
+                <a href="">learn in 1h</a>
+              </li>
+            </ul> */}
+          </li>
+          <li className="main-list">
+            <a className="main-link" href="/css">
+              CSS
+            </a>
+            {/* <ul className="sub-ul">
+              <li>
+                <a href="">Full Course</a>
+              </li>
+              <li>
+                <a href="">CSS Examples</a>
+              </li>
+              <li className="mini-projects">
+                <a href="">mini projects&nbsp; + </a>
+                <ul className="sub-sub-ul">
+                  <li>
+                    <a href="">project 1</a>
+                  </li>
+                  <li>
+                    <a href="">project 2</a>
+                  </li>
+                  <li>
+                    <a href="">project 3</a>
+                  </li>
+                </ul>
+              </li>
+            </ul> */}
+          </li>
+          <li className="main-list">
+            <a className="main-link" href="/javascript">
+              JavaScript
+            </a>
+            {/* <ul className="sub-ul sub-of-js">
+              <li>
+                <a href="">coming soon🔥</a>
+              </li>
+            </ul> */}
+          </li>
+        </ul>
+      </header>
+
+      {/* <header style={{ backgroundColor: "red" }} className="show-when-mobile">
+        <h1>c4a.dev **</h1>
+        <label className="absolute" htmlFor="burger">
+          <i className="fas fa-bars" />
+        </label>
+        <input id="burger" type="checkbox" />
+        <div className="show-on-click">
+          <div className="main-div">
+            <label htmlFor="html">
+              HTML <i className="fas fa-plus" />
+            </label>
+            <input id="html" type="checkbox" />
+            <ul className="sub-div">
+              <li>
+                <a href="">Full Course</a>
+              </li>
+              <li>
+                <a href="">Crash Course</a>
+              </li>
+              <li>
+                <a href="">learn in 1h</a>
+              </li>
+            </ul>
+          </div>
+          <div className="main-div">
+            <label htmlFor="css">
+              CSS <i className="fas fa-plus" />
+            </label>
+            <input id="css" type="checkbox" />
+            <ul className="sub-div">
+              <li>
+                <a href="">Full Course</a>
+              </li>
+              <li>
+                <a href="">CSS Examples</a>
+              </li>
+              <li>
+                <label className="mini-projects" htmlFor="mini">
+                  mini projects <i className="fas fa-plus" />
+                </label>
+                <input id="mini" type="checkbox" />
+                <ul className="sub-sub-div">
+                  <li>
+                    <a href="">project 1</a>
+                  </li>
+                  <li>
+                    <a href="">project 2</a>
+                  </li>
+                  <li>
+                    <a href="">project 3</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="main-div">
+            <label htmlFor="js">
+              JavaScript <i className="fas fa-plus" />
+            </label>
+            <input id="js" type="checkbox" />
+            <ul className="sub-div">
+              <li>
+                <a href="">coming soon🔥</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header> */}
+    </div>
+  );
+};
+
+export default Header;
