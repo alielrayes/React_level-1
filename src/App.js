cd9b5dbd6a269f37ee99869ea682eb4a802c7df3
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-10-22 02:53:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
-import Html from "./pages/html";
-import Css from "./pages/css";
-import Javascript from "./pages/javascript";
+ 
 
 function App() {
-  return (
-    <div className="App">
-   
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/html" element={<Html />} />
-        <Route path="/css" element={<Css />} />
-        <Route path="/javascript" element={<Javascript />} />
-      </Routes>
-    </div>
-  );
+  return <div>App.js file</div>;
 }
 
 export default App;
