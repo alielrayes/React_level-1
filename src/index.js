6e2d5f66b610dbbc7aa81a95d6c2d42df5982294
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-10-22 02:53:08 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import {   HelmetProvider } from "react-helmet-async";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import HTML from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascript";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>SORROY.........</h1>,
+  },
+
+  {
+    path: "/html",
+    element: <HTML />,
+  },
+
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
